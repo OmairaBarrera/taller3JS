90957c6c293d83ae4edfb758849e1a972cd0d6cc
Omaira Barrera <lomaira.barrera@gmail.com>
2023-03-15 09:36:12 -0500

    feat: :sparkles: layout of forms
    forms for each items.

--- js/main.js
+++ js/main.js
@@ -1,2 +1,35 @@
 /* TALLER 3 JAVASCRIPT CON DOM
- */
+
+EJERCICIO 1: UN SOLO FORMULARIO OBJETO DENTRO DE OTROS OBJETOS, ESTRUCTURA DE DATOS
+
+Construir un objeto literal(ciclo) "campus" que gestione
+la info(PROPIEDADES) de Campus, trainers, campers, niveles,
+tecnologías, teams y roadMap(temario)
+1.1. De campus administrar los datos de contacto de las sedes en
+Bucaramanga, Bogotá, Medellín y México
+1.2. De los trainers y campers, su nombre, sus teléfonos, teams
+(horarios de las teams=> día, hora y salones (nro y piso), y el
+email, y de los campers también horarios de inglés y ser.(formulario DOM)
+1.3. De los campers, también gestionar su nivel actual, como su
+barrio y medio de transporte
+1.4. De los niveles, su pre requisito, a que tecnología pertenece, si
+es electiva u obligatoria
+1.5. De la roadmap , Nro de créditos, año, Nro de asignaturas
+
+eventos de errores
+*/
+
+let campus = {};
+
+//FORMULARIO SEDES
+//FORMULARIO DE TEAMS
+//FORMULARIO DE TECNOLOGIAS
+//LISTA DE TECNOLOGIAS
+//FORMULARIO DE NIVELES
+//LISTA DE SEDES
+//LISTA DE TEAMS
+//LISTA DE NIVELES
+//FORMULARIO CAMPERS
+//FORMULARIO TRAINERS
+//FORMULARIO DE ROADMAP
+
